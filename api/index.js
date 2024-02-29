defc9a2a15f41b694ccb35198bca2231fceee26a
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-29 15:38:17 +0530

    bug fix

--- api/index.js
+++ api/index.js
@@ -31,7 +31,9 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
 
