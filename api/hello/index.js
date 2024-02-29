6f8e5b53dd492169b583fc5496611cc3aaf74a92
Vivek Shukla <vivekshuklahere@gmail.com>
2024-02-29 13:27:52 +0530

    added code

--- api/hello/index.js
+++ api/hello/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel workd!"));
+app.get("/", (req, res) => res.send("Express on Vercel worked!"));
 
 app.listen(4000, () => console.log("Server ready on port 3000."));
 
